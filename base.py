462cffbb44d572ec5aa74984568c4cb7b48b3416
MELVIN\Melvin Suvajev <suvajev@gmail.com>
2026-01-12 17:54:24 +0000

    Implemented and set up model system and connections (using comyx library) ), need to add algorithm to determine coefficient matrices and beamforming of BS

--- base.py
+++ base.py
@@ -1,1 +1,65 @@
-print("Hello World!")
+from comyx.network import (
+    UserEquipment,
+    BaseStation,
+    STAR_RIS,
+    Link,
+    effective_channel_gain,
+)
+from comyx.fading import Rayleigh
+from comyx.propagation import get_noise_power
+from comyx.utils import dbm2pow, get_distance, generate_seed, db2pow
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+Pmax = 20 #insert max value (dBm)
+Pt = np.linspace(0, Pmax, 1) # dBm
+Pt_lin = dbm2pow(Pt) # Watt
+Bw = 180e3 #Bandwidth (Hz)
+mc = 32 #Number of channel realizations
+K = 10 #Rician factors
+fc = 2.4e9 #carrier frequency
+temperature = 300 # Kelvin
+
+N0 = -80 #power of AWGN (dBm)
+
+M = 4 #number of antenna at the BS
+N = 30 #number of  STAR-RIS elements
+
+los_fading = {"type": "rayleigh", "sigma": 1 / 2} #G(n)LoS & h(n)LoS componenets both follow Rayleigh fading
+
+br_pathloss_args = {"type": "reference", "alpha": 2.2, "p0": -30, "frequency": fc}
+ru_pathloss_args = {"type": "reference", "alpha": 2.5, "p0": -30, "frequency": fc}
+
+BS = BaseStation("BS", position = [0, 0, 0], n_antennas = M, t_power = Pt_lin)
+SR = STAR_RIS("SR", position = [30, 40, 0], n_elements = N)
+
+UE1 = UserEquipment("UER", position = [34, 37, 0], n_antennas = 1)
+UE2 = UserEquipment("UET", position = [38, 46, 0], n_antennas = 1)
+
+# Shapes for channels
+shape_br = (N, M, mc)
+shape_ru = (N, 1, mc)
+
+# Links
+
+link_bs_starris = Link(
+    BS, SR,
+    los_fading, br_pathloss_args,
+    shape = shape_br, seed = generate_seed("STAR_RIS-BS")
+)
+
+link_starris_uer = Link(
+    SR, UE1,
+    los_fading, br_pathloss_args,
+    shape = shape_ru, seed = generate_seed("STAR_RIS-UE1"),
+    rician_args = {"K": db2pow(K), "order":"pre"} 
+)
+
+
+link_starris_uet = Link(
+    SR, UE2,
+    los_fading, br_pathloss_args,
+    shape = shape_ru, seed = generate_seed("STAR_RIS-UE1"),
+    rician_args = {"K": db2pow(K), "order":"pre"} 
+)
