f5b6680568ee282b3dad5d8a74672e69f9a76307
MELVIN\Melvin Suvajev <suvajev@gmail.com>
2026-01-13 18:57:47 +0000

    barely working form of program that will output a graph containing the Average EE vs the transmit power of the base station, will require a rewrite of earlier hunks

--- base.py
+++ base.py
@@ -1,7 +1,7 @@
 from comyx.network import (
     UserEquipment,
     BaseStation,
-    STAR_RIS,
+    STAR_RIS, RIS,
     Link,
     cascaded_channel_gain,
 )
@@ -10,12 +10,15 @@ from comyx.propagation import get_noise_power
 from comyx.utils import dbm2pow, get_distance, generate_seed, db2pow
 
 import numpy as np
+from numba import jit
 from matplotlib import pyplot as plt
 
 CRt = 0 #Achievable communication rate for User on transmitter side
 CRr = 0 #Achievable communication rate for User on reflector side
+EE_t = np.zeros(20)
+EE_r = np.zeros(20)
 Pmax = 20 #insert max value (dBm)
-Pt = np.linspace(0, Pmax, 1) # dBm
+Pt = np.linspace(0, Pmax, 20) # dBm
 Pt_lin = dbm2pow(Pt) # Watt
 Bw = 180e3 #Bandwidth (Hz)
 mc = 32 #Number of channel realizations
@@ -24,6 +27,7 @@ fc = 2.4e9 #carrier frequency
 temperature = 300 # Kelvin
 
 N0 = -80 #power of AWGN (dBm)
+N0_lin = dbm2pow(N0)
 
 M = 4 #number of antenna at the BS
 N = 30 #number of  STAR-RIS elements
@@ -34,14 +38,20 @@ br_pathloss_args = {"type": "reference", "alpha": 2.2, "p0": -30, "frequency": f
 ru_pathloss_args = {"type": "reference", "alpha": 2.5, "p0": -30, "frequency": fc}
 
 BS = BaseStation("BS", position = [0, 0, 0], n_antennas = M, t_power = Pt_lin)
-SR = STAR_RIS("SR", position = [30, 40, 0], n_elements = N)
+STr = RIS("STr", position = [30, 40, 0], n_elements = N)
+STt = RIS("STt", position = [30, 40, 0], n_elements = N)
 
 shape_starris = (N)
 
+STr.phase_shifts = np.zeros(N)
+STr.amplitudes = np.ones(N)
+STr.amplitudes.fill(0.5)
 #SR.reflection_phases = np.zeros(N) #initialise reflection phase shifts
 #SR.reflection_amplitudes = np.ones(N) #initialise reflection amplitudes
 #SR.reflection_amplitudes.fill(0.5)
-
+STt.phase_shifts = np.zeros(N)
+STt.amplitudes = np.ones(N)
+STt.amplitudes.fill(0.5)
 #SR.transmission_phases = np.zeros(N) #initialise transmission phase shifts
 #SR.transmission_amplitudes = np.ones(N) #initialise transmission amplitudes
 #SR.transmission_amplitudes.fill(0.5)
@@ -52,18 +62,25 @@ UET = UserEquipment("UE2", position = [38, 46, 0], n_antennas = 1)
 
 # Shapes for channels
 shape_br = (N, M)
-shape_ru = (N, 1)
+shape_ru = (N, M)
 
 # Links
 
-link_bs_starris = Link(
-    BS, SR,
+link_bs_starris_r = Link(
+    BS, STr,
     los_fading, br_pathloss_args,
     shape = shape_br, seed = generate_seed("STAR_RIS-BS")
 )
 
+link_bs_starris_t = Link(
+    BS, STt,
+    los_fading, br_pathloss_args,
+    shape = shape_br, seed = generate_seed("STAR_RIS-BS")
+)
+
+
 link_starris_uer = Link(
-    SR, UER,
+    STr, UER,
     los_fading, br_pathloss_args,
     shape = shape_ru, seed = generate_seed("STAR_RIS-UE1"),
     rician_args = {"K": db2pow(K), "order":"pre"} 
@@ -71,11 +88,49 @@ link_starris_uer = Link(
 
 
 link_starris_uet = Link(
-    SR, UET,
+    STt, UET,
     los_fading, br_pathloss_args,
    shape = shape_ru, seed = generate_seed("STAR_RIS-UE1"),
     rician_args = {"K": db2pow(K), "order":"pre"} 
 )
 
-#channel_gain_UER = cascaded_channel_gain(link_bs_starris, link_starris_uer, style = "matrix")
-#channel_gain_UET = cascaded_channel_gain(link_bs_starris, link_starris_uet, style = "matrix")
+channel_gain_UER = cascaded_channel_gain(link_bs_starris_r, link_starris_uer, style = "matrix")
+#assert channel_gain_UER.shape == link_starris_uer, "Shapes do not match"
+channel_gain_UET = cascaded_channel_gain(link_bs_starris_t, link_starris_uet, style = "matrix")
+#assert channel_gain_UET.shape == link_starris_uet, "Shapes do not match"
+
+#Magnitude of channel gains
+mag_UER = np.abs(channel_gain_UER) ** 2
+mag_UET = np.abs(channel_gain_UET) ** 2
+
+print("power of basestation is" , BS.t_power)
+
+
+CRr = np.log2(1 +  mag_UER/(mag_UER+N0_lin))
+
+#print("achievable comms rate of reflection is", CRr)
+
+CRt = np.log2(1 +  mag_UET/(mag_UET+N0_lin))
+
+for i in range(20):
+    p = BS.t_power[i]
+    total_com = sum(CRt)
+    total_com = sum(total_com)
+    EE_t[i] = (Bw * total_com) / p
+
+for i in range(20):
+    p = BS.t_power[i]
+    total_com = sum(CRr)
+    total_com = sum(total_com)
+    EE_r[i] = (Bw * total_com) / p
+
+
+
+plt.plot(Pt, EE_r)
+plt.xlabel("Transmit power (dBm)")
+plt.ylabel("Energy efficiency (kBits/Joule)")
+plt.savefig("testgraph", dpi = 300, bbox_inches = "tight")
+
+
+print("energy efficiency of transmission is " , EE_t)
+print("energy efficiency of reflection is " , EE_r)
