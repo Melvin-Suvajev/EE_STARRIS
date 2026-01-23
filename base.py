111d4e08f0d35887994b99bc504e6061ef048428
MELVIN\Melvin Suvajev <suvajev@gmail.com>
2026-01-23 12:09:04 +0000

    Created STAR_RIS class in order to define parameters and initialise matrices that will be used when running simulation, as well as intergratiing class with functions that generate the fading channels between BS, STAR-RIS and users.

--- base.py
+++ base.py
@@ -1,203 +1,67 @@
-'''
-from comyx.network import (
-    UserEquipment,
-    BaseStation,
-    STAR_RIS, RIS,
-    Link,
-    cascaded_channel_gain,
-)
-
-from comyx.propagation import get_noise_power
-from comyx.utils import dbm2pow, get_distance, generate_seed, db2pow
-
 import numpy as np
-from numba import jit
-
-
-CRt = 0 #Achievable communication rate for User on transmitter side
-CRr = 0 #Achievable communication rate for User on reflector side
-EE_t = np.zeros(20)
-EE_r = np.zeros(20)
-Pmax = 20 #insert max value (dBm)
-Pt = np.linspace(0, Pmax, 20) # dBm
-Pt_lin = dbm2pow(Pt) # Watt
-Bw = 180e3 #Bandwidth (Hz)
-mc = 32 #Number of channel realizations
-K = 10 #Rician factors
-fc = 2.4e9 #carrier frequency
-temperature = 300 # Kelvin
-
-N0 = -80 #power of AWGN (dBm)
-N0_lin = dbm2pow(N0)
-
-N = 4 #number of antenna at the BS
-M = 30 #number of  STAR-RIS elements
-
-los_fading = {"type": "rayleigh", "sigma": 1 / 2} #G(n)LoS & h(n)LoS componenets both follow Rayleigh fading
-
-br_pathloss_args = {"type": "reference", "alpha": 2.2, "p0": -30, "frequency": fc}
-ru_pathloss_args = {"type": "reference", "alpha": 2.5, "p0": -30, "frequency": fc}
-
-BS = BaseStation("BS", position = [0, 0, 0], n_antennas = M, t_power = Pt_lin)
-STr = RIS("STr", position = [30, 40, 0], n_elements = N)
-STt = RIS("STt", position = [30, 40, 0], n_elements = N)
-
-shape_starris = (N)
-
-STr.phase_shifts = np.zeros(N)
-STr.amplitudes = np.ones(N)
-STr.amplitudes.fill(0.5)
-#SR.reflection_phases = np.zeros(N) #initialise reflection phase shifts
-#SR.reflection_amplitudes = np.ones(N) #initialise reflection amplitudes
-#SR.reflection_amplitudes.fill(0.5)
-STt.phase_shifts = np.zeros(N)
-STt.amplitudes = np.ones(N)
-STt.amplitudes.fill(0.5)
-#SR.transmission_phases = np.zeros(N) #initialise transmission phase shifts
-#SR.transmission_amplitudes = np.ones(N) #initialise transmission amplitudes
-#SR.transmission_amplitudes.fill(0.5)
-
-
-UER = UserEquipment("UE1", position = [34, 37, 0], n_antennas = 1)
-UET = UserEquipment("UE2", position = [38, 46, 0], n_antennas = 1)
-
-# Shapes for channels
-shape_br = (N, M)
-shape_ru = (N, M)
-
-# Links
-
-link_bs_starris_r = Link(
-    BS, STr,
-    los_fading, br_pathloss_args,
-    shape = shape_br, seed = generate_seed("STAR_RIS-BS")
-)
-
-link_bs_starris_t = Link(
-    BS, STt,
-    los_fading, br_pathloss_args,
-    shape = shape_br, seed = generate_seed("STAR_RIS-BS")
-)
-
-
-link_starris_uer = Link(
-    STr, UER,
-    los_fading, br_pathloss_args,
-    shape = shape_ru, seed = generate_seed("STAR_RIS-UE1"),
-    rician_args = {"K": db2pow(K), "order":"pre"} 
-)
-
-
-link_starris_uet = Link(
-    STt, UET,
-    los_fading, br_pathloss_args,
-   shape = shape_ru, seed = generate_seed("STAR_RIS-UE1"),
-    rician_args = {"K": db2pow(K), "order":"pre"} 
-)
-
-channel_gain_UER = cascaded_channel_gain(link_bs_starris_r, link_starris_uer, style = "matrix")
-#assert channel_gain_UER.shape == link_starris_uer, "Shapes do not match"
-channel_gain_UET = cascaded_channel_gain(link_bs_starris_t, link_starris_uet, style = "matrix")
-#assert channel_gain_UET.shape == link_starris_uet, "Shapes do not match"
-
-#Magnitude of channel gains
-mag_UER = np.abs(channel_gain_UER) ** 2
-mag_UET = np.abs(channel_gain_UET) ** 2
-
-print("power of basestation is" , BS.t_power)
-
-
-CRr = np.log2(1 +  mag_UER/(mag_UER+N0_lin))
-
-#print("achievable comms rate of reflection is", CRr)
-
-CRt = np.log2(1 +  mag_UET/(mag_UET+N0_lin))
-
-for i in range(20):
-    p = BS.t_power[i]
-    total_com = sum(CRt)
-    total_com = sum(total_com)
-    EE_t[i] = (Bw * total_com) / p
-
-for i in range(20):
-    p = BS.t_power[i]
-    total_com = sum(CRr)
-    total_com = sum(total_com)
-    EE_r[i] = (Bw * total_com) / p
-
-
-
-plt.plot(Pt, EE_r)
-plt.xlabel("Transmit power (dBm)")
-plt.ylabel("Energy efficiency (kBits/Joule)")
-plt.savefig("testgraph", dpi = 300, bbox_inches = "tight")
-
-
-print("energy efficiency of transmission is " , EE_t)
-print("energy efficiency of reflection is " , EE_r)
-
-
-'''
-
-
-#attempt to rebuild without comyx library
-import numpy as np
-import math as math
+import random 
 from matplotlib import pyplot as plt
+from dataclasses import dataclass
 
-M = 30 #number of elements in the STAR-RIS
-N = 10 #number of antennae in the BS
-P_c = 40 #total power comsumption (dBm)
+@dataclass  
+class STAR_RIS:
 
-G = np.zeros((M,N), dtype='float32') #channel between BS and STAR-RIS
-v_ut = np.zeros((M, 1), dtype='float32') #channel between UT and STAR-RIS
-v_ur = np.zeros((M, 1), dtype='float32') #channel between UR and STAR-RIS
+    M: int #number of elements in the STAR-RIS
+    N: int #number of antennae in the BS
 
-G_nlos = np.zeros((M,N), dtype='float32') #nLos components of G channel 
-v_nlos_i = np.zeros((M,1), dtype='float32') #nLos components of v channels
+    def __init__(self, M, N):
+        self.elements = M
+        self.antennae = N
+        self.G = np.zeros((M, N))
+        self.v_ut = np.zeros((M, 1))
+        self.v_ur = np.zeros((M, 1))
+        self.PHI_T = np.zeros(M) #transmission coefficients
+        self.PHI_R = np.zeros(M) #reflection coefficients
+        self.G_nlos = np.zeros((M, N))
+        self.G_los = np.ones((M, N))
+        self.v_nlos_i = np.zeros((M, 1))
+        self.v_los_i = np.ones((M, 1))
+        self.p_0_dB: int = -30  #path loss at reference distance 1m (dB)
+        self.p_0: float = 10**(self.p_0_dB/10) #conversion to amplitude
+        self.d_G: float = 50 #distance between BS and STAR-RIS (m)
+        self.a_BR: float = 2.2 #path loss exponent between BS and STAR-RIS
+        self.a_RU: float = 2.5 #path loss exponent between STAR-RIS and user
+        self.K_BR: float = 10 #Rician factor between BS and STAR-RIS
+        self.K_RU: float = 10 #Rician factor between STAR-RIS and user
+        self.d_ut: float = 5 #distance between STAR-RIS and user on transmission side (m)
+        self.d_ur: float = 10 #distance between STAR-RIS and user on reflection side (m)
 
-o_2_dBm = -80 #power of AWGN (dBm)
-o_2 = (10**(-80/10)) #power of AWGN (W)
-p_0_dB = -30 #path loss at reference distance 1m (dB)
-p_0 = 10**(-30/10) #conversion to amplitude
-d_G = 50 #distance between BS and STAR-RIS (m)
-a_BR = 2.2 #path loss exponent between BS and STAR-RIS
-a_RU = 2.5 #path loss exponent between STAR-RIS and user
-K_BR = 10 #Rician factor between BS and STAR-RIS
-K_RU = 10 #Rician factor between STAR-RIS and user
-
-d_ut = 5 #distance between STAR-RIS and user on transmission side (m)
-d_ur = 10 #distance between STAR-RIS and user on reflection side (m)
-
-PHI_T = np.zeros(M) #transmission coefficients
-PHI_R = np.zeros(M) #reflection coefficients
-
-#print("this is the G matrix", G)
-#print("and this is the v matrix", v_ut)
-
-#function to generate G and v_i
-def rayleigh_fading():
-    for i in range(M):
-        for j in range(N):
-            G_nlos[i,j] = 1 - np.exp(-(j**2)/(2)) #defining the channel between each element of the STAR RIS and each antenna of the BS
-           # print("j is currently", j)
-           # print("answer this time is ", G_nlos[i,j])
-            
-       # print("i is currently", i)
+def rayleigh_fading(STAR_RIS):
+        #print("variance was ", var)
+        for i in range(STAR_RIS.elements):
+            var = random.uniform(0.5, 10)  #variance of rayleigh distribution
+            for j in range(STAR_RIS.antennae):
+                STAR_RIS.G_nlos[i,j] = 1 - np.exp(-(j**2)/(2*(var**2))) #defining the channel between each element of the STAR RIS and each antenna of the BS
+            # print("j is currently", j)
+            # print("answer this time is ", G_nlos[i,j])
+        for i in range(STAR_RIS.elements):
+            STAR_RIS.v_nlos_i[i,:] = 1 - np.exp((-(i**2)/(2*(var**2)))) #defining the channel between each user and each element       
+        # print("i is currently", i)
         
+   
+def rician_channel(STAR_RIS):
+    rayleigh_fading(STAR_RIS)
+    STAR_RIS.G = np.sqrt((STAR_RIS.p_0/((STAR_RIS.d_G)**STAR_RIS.a_BR)))*((np.sqrt((STAR_RIS.K_BR/(1+STAR_RIS.K_BR)))*STAR_RIS.G_los)+(np.sqrt((1/(1+STAR_RIS.K_BR)))*STAR_RIS.G_nlos))
+    STAR_RIS.v_ut = np.sqrt((STAR_RIS.p_0/((STAR_RIS.d_ut)**STAR_RIS.a_RU)))*(((np.sqrt((STAR_RIS.K_RU/(1+STAR_RIS.K_RU))))*STAR_RIS.v_los_i)+(np.sqrt((1/(1+STAR_RIS.K_RU)))*STAR_RIS.v_nlos_i))
+    STAR_RIS.v_ur = np.sqrt((STAR_RIS.p_0/((STAR_RIS.d_ur)**STAR_RIS.a_RU)))*((np.sqrt((STAR_RIS.K_RU/(1+STAR_RIS.K_RU)))*STAR_RIS.v_los_i)+(np.sqrt((1/(1+STAR_RIS.K_RU)))*STAR_RIS.v_nlos_i))
 
-    for i in range(M):
-        v_nlos_i[i,:] = 1 - np.exp((-(i**2)/(2))) #defining the channel between each user and each element
-        
-    
-'''
-def rician_channel():
-    G = np.sqrt()
-    return
-'''
-#rayleigh_fading()
-#sanity_check = 1 - np.exp(-1/2)
+ 
 
-#print("LoS of G is", G_nlos)
-#print("LoS of v is" ,v_nlos_i)
-#mergetest
+
+def main():
+    #print("Hello Maks")
+    instance = STAR_RIS(30, 10)
+    rician_channel(instance)
+    print("G is defined as" , instance.G)
+    print("\nv_ut is defined as" , instance.v_ut)
+    print("\nv_ur is defined as" , instance.v_ur)
+
+
+print("Hello Melvin")
+if __name__ == "__main__":
+    main()
